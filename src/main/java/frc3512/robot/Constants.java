d81b8ba377b32d60cf3b17e7b72666c20d3bfd93
TreeWonTwoToFor <jonathan-github@shuman.org>
2023-01-31 19:41:44 -0800

    Made Elevator and Intake code

--- src/main/java/frc3512/robot/Constants.java
+++ src/main/java/frc3512/robot/Constants.java
@@ -92,6 +92,26 @@ public final class Constants {
   public static final class AutonConstants {
     public static final double maxVelocity = 1.0;
     public static final double maxAcceleration = 4.0;
+  public static final class ElevatorConstants {
+    public static final double kDt = 0.02;
+
+    public static final int EncoderA = 0;
+    public static final int EncoderB = 1;
+    public static final int MotorAID = 0;
+    public static final int MotorBID = 1;
+
+    public static final double distancePerpulse = 0.0;
+  }
+
+  public static final class IntakeConstants {
+    public static final int MotorID = 0;
+  }
+
+  /** Constants revolving around the swerve subsystem */
+  public static final class SwerveConstants {
+    /* Gyro Constants */
+    public static final int pigeonID = 6;
+    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
     public static final PathConstraints constraints =
         new PathConstraints(maxVelocity, maxAcceleration);
---
+++ src/main/java/frc3512/robot/subsystems/Elevator.java
@@ -0,0 +1,41 @@
+package frc3512.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc3512.lib.util.CANSparkMaxUtil;
+import frc3512.robot.Constants.ElevatorConstants;
+
+public class Elevator extends SubsystemBase {
+    private static double kDt = ElevatorConstants.kDt;
+
+    private final Encoder m_Encoder = new Encoder(ElevatorConstants.EncoderA, ElevatorConstants.EncoderB);
+    private final MotorController m_motor = new CANSparkMax(ElevatorConstants.MotorAID, MotorType.kBrushless); 
+
+    private final TrapezoidProfile.Constraints m_Constraints = 
+        new TrapezoidProfile.Constraints(kDt, kDt);
+    private final ProfiledPIDController m_controller =
+        new ProfiledPIDController(kDt, kDt, kDt, m_Constraints);
+
+    public void setup() {
+        m_Encoder.setDistancePerPulse(ElevatorConstants.distancePerpulse);
+    }
+
+    public void controllerPeriodic() {
+        m_motor.set(m_controller.calculate(m_Encoder.getDistance()));
+    }
+
+    public void setGoal(double goalNumber) {
+        m_controller.setGoal(goalNumber);
+    }
+
+    @Override
+    public void periodic() {
+        controllerPeriodic(); 
+    }
+}
