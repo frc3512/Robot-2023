d06d232d6af8ad7faa0b151e6ccb0ee43abe1280
TreeWonTwoToFor <jonathan-github@shuman.org>
2023-03-06 19:02:48 -0800

    changed super structure and autoDrop

--- src/main/java/frc3512/robot/commands/AutoDrop.java
+++ src/main/java/frc3512/robot/commands/AutoDrop.java
@@ -4,37 +4,44 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc3512.robot.commands.DriveToPose;
 import frc3512.robot.subsystems.Arm;
 import frc3512.robot.subsystems.Elevator;
 import frc3512.robot.subsystems.Intake;
+import frc3512.robot.subsystems.Superstructure;
 
 public class AutoDrop {
   private Intake intake = new Intake();
   private Arm arm = new Arm();
   private Elevator elevator = new Elevator();
-
-  public Command automaticIntake(int gamePiece) {
-    double speed;
-    if (gamePiece == 0) {
-      speed = 0.65;
-    } else if (gamePiece == 1) {
-      speed = -0.65;
-    }
+  
+  public Command automaticGroundIntake() {
     return (
         // arm to the horizontal potition
-        arm.setGoal(0.0, 0.0)
+        arm.setGoal()
       ).andThen(
         // elevator to the ground position
         elevator.setGoal(new TrapezoidProfile.State(0.0, 0.0))
       ).andThen(
-        // intake the game piece, 0 is cube, 1 is cone
-        intake.customIntake(speed)
+        intake.intakeGamePiece()
       );
+  }
+
+  public Command automaticHPIntake() {
+    return (
+        // arm to the horizontal potition
+        arm.setGoal(new TrapezoidProfile.State(0.08, 0.0)
+      ).andThen(
+        // elevator to the ground position
+        elevator.setGoal(new TrapezoidProfile.State(0.35, 0.0))
+      ).andThen(
+        intake.intakeGamePiece()
+      ));
   }
 
   public void automaticScoring(double height, int station, int gamePiece) {
     if (station == 0) {
-      // go to the right station
+      // go to the right station DriveToPose
     } else if (station == 1) {
       // go to the middle station
     } else if (station == 2) {
@@ -42,15 +49,15 @@ public class AutoDrop {
     }
 
     // drive elevator to the height
+    elevator.setGoal((new TrapezoidProfile.State(height, 0.0)));
 
-    if (gamePiece == 0) {
-      intake.outtakeCube();
-    } else if (gamePiece == 1) {
-      intake.outtakeCone();
-    }
+    intake.outtakeGamePiece();
   }
 
-  public Command scoreGamePiece(double level) {
-    return Commands.sequence(elevator.setGoal(new TrapezoidProfile.State(level, 0.0)), arm.setGoal());
+  public Command setupScoring(double elevatorHeight, double armHeight) {
+    return Commands.sequence(
+      elevator.setGoal(new TrapezoidProfile.State(elevatorHeight, 0.0)), 
+      arm.setGoal((new TrapezoidProfile.State(armHeight, 0.0))
+      );
   }
 }
